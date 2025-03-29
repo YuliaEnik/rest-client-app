f35bb5f994b9c6c96a0a9525b2505c9a91f4ea50
Nikolay Makarevich <49314579+NMakarevich@users.noreply.github.com>
2025-03-29 23:44:45 +0300

    Restful layout (#13)
    * add select component
    
    * feat: #10 create routing app, layout (#11)
    
    * feat: add restful page layout
    
    * refactor: move types to types.tsx, constants to constants.tsx, add public api
    
    ---------
    
    Co-authored-by: YuliaEnik <70852362+YuliaEnik@users.noreply.github.com>

---
+++ src/components/restful/select-method/select-method.tsx
@@ -0,0 +1,20 @@
+'use client';
+
+import { SelectElement } from '@/components/restful/select';
+import { RESTFUL_METHODS } from '@/constants/constants';
+import { METHODS, Option } from '@/types/types';
+
+const METHODS_OPTIONS: Option[] = RESTFUL_METHODS.map((method) => ({
+  value: method,
+  label: method,
+}));
+
+export function SelectMethod({ currentMethod }: { currentMethod: METHODS }) {
+  return (
+    <SelectElement
+      options={METHODS_OPTIONS}
+      initialValue={currentMethod}
+      handleChangeAction={(value) => console.log(value)}
+    />
+  );
+}
