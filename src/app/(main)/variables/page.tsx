e74d66341859a78ee63c78c797b8a751b96b0f26
Anastasiya Andronova <anastasiya5.88@mail.ru>
2025-04-02 00:17:15 +0300

    feat: add edit, save variables

--- src/app/(main)/variables/page.tsx
+++ src/app/(main)/variables/page.tsx
@@ -1,11 +1,12 @@
 'use client';
-import { useState } from 'react';
-import { PlusIcon } from 'lucide-react';
+import { useEffect, useState } from 'react';
+import { Edit2, PlusIcon, Save, SquareX, Trash2 } from 'lucide-react';
 
 import { Button } from '@/components/ui/button';
 import { Input } from '@/components/ui/input';
 
 interface Variable {
+  id: string;
   name: string;
   value: string;
 }
@@ -17,6 +18,27 @@ export default function VariablesPage() {
   const [variables, setVariables] = useState<Variable[]>([]);
   const [error, setError] = useState('');
   const [editingIndex, setEditingIndex] = useState<number | null>(null);
+  const [editingId, setEditingId] = useState<string | null>(null);
+  const [nextId, setNextId] = useState(1);
+
+  useEffect(() => {
+    const storedVariables = localStorage.getItem('variables');
+    if (storedVariables) {
+      const parsedVariables: Variable[] = JSON.parse(storedVariables);
+      setVariables(parsedVariables);
+      if (parsedVariables.length > 0) {
+        const lastId = parsedVariables[parsedVariables.length - 1].id;
+        const lastIdNumber = Number(lastId.slice(1));
+        setNextId(lastIdNumber + 1);
+      } else {
+        setNextId(1);
+      }
+    }
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem('variables', JSON.stringify(variables));
+  }, [variables]);
 
   const handleAddVariableClick = () => {
     setShowInputBlock(true);
@@ -36,20 +58,23 @@ export default function VariablesPage() {
       return;
     }
 
+    const newVariable: Variable = {
+      id: editingIndex === null ? `#${nextId}` : `${editingId}`,
+      name: variableName,
+      value: variableValue,
+    };
+
     if (editingIndex !== null) {
       setVariables((prev) => {
         const updatedVariables = [...prev];
-        updatedVariables[editingIndex] = {
-          name: variableName,
-          value: variableValue,
-        };
+        updatedVariables[editingIndex] = newVariable;
         return updatedVariables;
       });
     } else {
-      setVariables((prev) => [
-        ...prev,
-        { name: variableName, value: variableValue },
-      ]);
+      setVariables((prev) => [...prev, newVariable]);
+      if (editingIndex === null) {
+        setNextId((prevId) => prevId + 1);
+      }
     }
 
     resetInputFields();
@@ -60,16 +85,21 @@ export default function VariablesPage() {
     const variableToEdit = variables[index];
     setVariableName(variableToEdit.name);
     setVariableValue(variableToEdit.value);
+    setEditingId(variableToEdit.id);
     setEditingIndex(index);
   };
 
   const handleSave = () => {
     handleAdd();
     setEditingIndex(null);
+    setEditingId(null);
   };
 
   const handleDelete = (index: number) => {
     setVariables((prev) => prev.filter((_, i) => i !== index));
+    if (variables.length === 0) {
+      setNextId(1);
+    }
   };
 
   return (
@@ -82,12 +112,12 @@ export default function VariablesPage() {
           variant="outline"
           onClick={handleAddVariableClick}
         >
-          <PlusIcon /> Add variable
+          <PlusIcon />
+          New variable
         </Button>
-
         {showInputBlock && (
           <div className="flex flex-col gap-4">
-            <div className="flex gap-4">
+            <div className="flex gap-2">
               <Input
                 placeholder="Name"
                 value={variableName}
@@ -99,64 +129,75 @@ export default function VariablesPage() {
                 onChange={(e) => setVariableValue(e.target.value)}
               />
               <Button
-                className="max-w-[100px] self-end"
+                className="max-w-[100px]"
                 type="button"
                 variant="outline"
                 onClick={handleAdd}
               >
-                {editingIndex !== null ? 'Update' : 'Add'}
+                <PlusIcon />
+              </Button>
+              <Button className="max-w-[100px]" type="button" variant="outline">
+                <SquareX />
               </Button>
             </div>
             {error && <p className="text-red-500">{error}</p>}
           </div>
         )}
         {variables.length > 0 && (
-          <div className="mt-4">
-            <h3>Added Variables:</h3>
+          <div className="flex flex-col gap-4">
+            <h3>Saved Variables:</h3>
             {variables.map((variable, index) => (
-              <div key={index} className="flex items-center gap-4">
+              <div key={index} className="flex flex-col gap-4">
                 {editingIndex === index ? (
-                  <div className="grid grid-cols-2 w-full">
-                    <div className="grid grid-cols-2">
-                      <Input
-                        value={variableName}
-                        onChange={(e) => setVariableName(e.target.value)}
-                      />
-                      <Input
-                        value={variableValue}
-                        onChange={(e) => setVariableValue(e.target.value)}
-                      />
-                    </div>
+                  <div className="flex gap-2 w-full">
+                    <p>{variable.id}</p>
+                    <Input
+                      placeholder="Name"
+                      value={variableName}
+                      onChange={(e) => setVariableName(e.target.value)}
+                    />
+                    <Input
+                      placeholder="Value"
+                      value={variableValue}
+                      onChange={(e) => setVariableValue(e.target.value)}
+                    />
                     <Button
-                      className="ml-2"
+                      className="max-w-[100px]"
                       type="button"
                       variant="outline"
                       onClick={handleSave}
                     >
-                      Save
-                    </Button>
-                  </div>
-                ) : (
-                  <div className="grid grid-cols-2 w-full">
-                    <div className="grid grid-cols-2">
-                      <p> {variable.name}:</p>
-                      <p> {variable.value}</p>{' '}
-                    </div>
-                    <Button
-                      className="ml-2"
-                      type="button"
-                      variant="outline"
-                      onClick={() => handleEdit(index)}
-                    >
-                      Edit
+                      <Save />
                     </Button>
                     <Button
-                      className="ml-2"
+                      className="max-w-[100px]"
                       type="button"
                       variant="outline"
                       onClick={() => handleDelete(index)}
                     >
-                      Delete
+                      <Trash2 />
+                    </Button>
+                  </div>
+                ) : (
+                  <div className="flex gap-2 w-full">
+                    <p>{variable.id}</p>
+                    <Input value={variable.name} disabled />
+                    <Input value={variable.value} disabled />
+                    <Button
+                      className="max-w-[100px]"
+                      type="button"
+                      variant="outline"
+                      onClick={() => handleEdit(index)}
+                    >
+                      <Edit2 />
+                    </Button>
+                    <Button
+                      className="max-w-[100px]"
+                      type="button"
+                      variant="outline"
+                      onClick={() => handleDelete(index)}
+                    >
+                      <Trash2 />
                     </Button>
                   </div>
                 )}
