022ce56d3d1a5baa15feff792b8b4d0094c4fd05
YuliaEnik <70852362+YuliaEnik@users.noreply.github.com>
2025-04-03 18:36:43 +0300

    #19 Header (#20)
    * feat: #10 create routing app, layout
    
    * feat: #12 create UI welcome page
    
    * refactor: change folders
    
    * refactor: change files for deploy
    
    * refactor:  install deploy
    
    * feat: #19 create header, logo, lang select
    
    * feat: add sticky
    
    * fix: fix comments in PR
    
    * fix:#19 fix name lang

--- src/components/layout/layout.tsx
+++ src/components/layout/layout.tsx
@@ -1,11 +1,11 @@
 import { Footer } from '@/components/shared/footer';
 
+import { Header } from '../shared/header/header';
+
 export const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
     <>
-      <header className="flex h-20 w-full justify-between flex-wrap p-4 gap-4 bg-secondary-rose">
-        <h2> Header</h2>
-      </header>
+      <Header />
       <main className="flex-1 w-full">{children}</main>
       <Footer />
     </>
