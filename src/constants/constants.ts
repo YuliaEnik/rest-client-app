cabd1ace5e605d5006c76baea0c6bc7e0186918d
NMakarevich <nikoladze529@mail.ru>
2025-04-11 01:09:54 +0300

    feat: add code generator

--- src/constants/constants.ts
+++ src/constants/constants.ts
@@ -1,4 +1,4 @@
-import { Option } from '@/types/types';
+import { lang, Option } from '@/types/types';
 
 export const RESTFUL_METHODS = [
   'GET',
@@ -10,12 +10,23 @@ export const RESTFUL_METHODS = [
 ] as const;
 
 export const LANGUAGES: Option[] = [
-  { value: 'fetch', label: 'JavaScript (Fetch API)' },
-  { value: 'xhr', label: 'JavaScript (XHR)' },
-  { value: 'curl', label: 'curl' },
-  { value: 'nodejs', label: 'NodeJS' },
-  { value: 'python', label: 'Python' },
-  { value: 'java', label: 'Java' },
-  { value: 'c#', label: 'C#' },
-  { value: 'go', label: 'Go' },
+  { value: 'javascript/Fetch', label: 'JavaScript (Fetch API)' },
+  { value: 'javascript/XHR', label: 'JavaScript (XHR)' },
+  { value: 'curl/cURL', label: 'cURL' },
+  { value: 'nodejs/Native', label: 'NodeJS' },
+  { value: 'python/http.client', label: 'Python' },
+  { value: 'java/OkHttp', label: 'Java' },
+  { value: 'csharp/HttpClient', label: 'C#' },
+  { value: 'go/Native', label: 'Go' },
 ];
+
+export const CODEMIRROR_LANGUAGES: Record<string, lang> = {
+  'javascript/Fetch': 'javascript',
+  'javascript/XHR': 'javascript',
+  'curl/cURL': 'text',
+  'nodejs/Native': 'javascript',
+  'python/http.client': 'python',
+  'java/OkHttp': 'java',
+  'csharp/HttpClient': 'csharp',
+  'go/Native': 'go',
+};
