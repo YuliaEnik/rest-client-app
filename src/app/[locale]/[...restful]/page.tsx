913d4c3ab082e6b062eb1f21a376e3ca80a03e5f
NMakarevich <nikoladze529@mail.ru>
2025-04-17 00:49:14 +0300

    fix: fix check for method

--- src/app/[locale]/[...restful]/page.tsx
+++ src/app/[locale]/[...restful]/page.tsx
@@ -1,7 +1,8 @@
 import { Suspense } from 'react';
 import dynamic from 'next/dynamic';
 import { redirect } from 'next/navigation';
-import { METHODS } from 'node:http';
+
+import { RESTFUL_METHODS } from '@/constants/constants';
 
 const RestfulView = dynamic(
   () => import('@/components/restful/restful-view/restful-view')
@@ -17,7 +18,10 @@ export default async function Page({
   const [method, ...url] = (await params).restful;
   const headers = await searchParams;
 
-  if (!METHODS.includes(method.toUpperCase())) redirect('/GET');
+  if (
+    !(RESTFUL_METHODS as ReadonlyArray<string>).includes(method.toUpperCase())
+  )
+    redirect('/GET');
 
   return (
     <section
