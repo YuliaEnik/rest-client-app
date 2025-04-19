24c241cae2ea57b37d775546842e3e352df14268
Anastasiya Andronava <106033630+anastan588@users.noreply.github.com>
2025-04-19 20:38:32 +0300

    History tests (#47)
    * feat: add history page tests
    
    * feat: add some tests
    
    * feat: add some test
    
    * feat: add profile description
    
    * fix: improve link

---
+++ src/tests/history-page/history_item.test.tsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import { fireEvent, render, screen, waitFor } from '@testing-library/react';
+import { afterEach, describe, expect, test, vi } from 'vitest';
+
+import { HistoryItem } from '@/components';
+
+vi.mock('next/navigation', () => ({
+  useRouter: vi.fn(),
+}));
+
+describe('HistoryItem Component', () => {
+  const mockPush = vi.fn();
+  const mockRequest = {
+    executedAt: 1744924534290,
+    restfulUrl: '/be/GET/aHR0cHM6Ly9hcGkucG90dGVyZGIuY29tL3YxL2NoYXJhY3RlcnM=?',
+    apiUrl: 'https://api.potterdb.com/v1/characters',
+  };
+
+  afterEach(() => {
+    vi.clearAllMocks();
+  });
+
+  test('renders HistoryItem correctly', () => {
+    render(<HistoryItem request={mockRequest} />);
+
+    expect(screen.getByText('Fri Apr 18 2025 12:15:34 AM')).toBeTruthy();
+    expect(screen.getByText('GET')).toBeTruthy();
+    expect(
+      screen.getByText('https://api.potterdb.com/v1/characters')
+    ).toBeTruthy();
+  });
+
+  test('navigates to the correct URL when clicked', () => {
+    render(<HistoryItem request={mockRequest} />);
+    waitFor(() => {
+      fireEvent.click(screen.getByTestId('link-button'));
+      expect(mockPush).toHaveBeenCalledWith(
+        '/be/GET/aHR0cHM6Ly9hcGkucG90dGVyZGIuY29tL3YxL2NoYXJhY3RlcnM=?'
+      );
+    });
+  });
+});
